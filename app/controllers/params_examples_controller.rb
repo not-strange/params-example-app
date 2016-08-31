8151d17fceb4a5e8a149fa12f9c0e8a73deae6fc
JackMarx <seriousfools@gmail.com>
2016-08-30 20:38:36 -0500

    url segment params

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -3,4 +3,45 @@ class ParamsExamplesController < ApplicationController
     @message = params[:coolio]
     @other_message = params[:other_message]
   end
+
+  def name
+    @name = params[:name].upcase
+  end
+
+  def name_starts_with_a
+    @a_name = params[:name]
+    if @a_name
+      if @a_name.starts_with?("a") || @a_name.starts_with?("A")
+        @message = "Your name start with an A."
+      end
+    end
+  end
+
+  def guess_query
+    guess = params[:guess]
+    if guess
+      guess = guess.to_i
+      correct_answer = 32
+
+      if guess == correct_answer
+        @message = "You Did It!!!!"
+      elsif guess < 1 || guess > 100
+        @message = "You're out of bounds, Guy."
+      elsif guess < correct_answer
+        @message = "Guess higher, buddy."
+      elsif guess > correct_answer
+        @message = "Guess lower, friend"
+      end
+    else
+      @message = "You obviously need to put something in the URL."
+    end
+  end
+
+  def url
+    @awkward = params[:turkey]
+  end
 end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get '/query_params_example' => 'params_examples#query'
+  get '/name' => 'params_examples#name'
+  get '/a_name' => 'params_examples#name_starts_with_a'
+  get '/guessing_game_query' => 'params_examples#guess_query'
+  get '/url_segment_example/:turkey' => 'params_examples#url'
 end
